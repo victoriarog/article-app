2d027e18aac8e1bd16989a9cf0bb6ce6bde84ca0
Vicka <vicrogatinskaya@gmail.com>
2024-01-26 19:01:15 +0100

    add: primary logic for article fetching

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function ArticleList({articles, onArticleClick}) {
   return (
     <div className='flex-item'>
-
+      <ul>
+        {articles.map((article, index) => (
+          <li key = {article.id} onClick={() => onArticleClick(article)}>
+            {article.title}
+            {article.undertitle}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
@@ -12,16 +19,39 @@ function ArticleList({articles, onArticleClick}) {
 function ArticleText({article}) {
   return (
     <div className='flex-item'>
-
+      {article && (
+        <div>
+          <h2>{article.title}</h2>
+          <p>{article.text}</p>
+        </div>
+      )
+      }
     </div>
   );
 }
 
 function App() {
+
+  const [articles, setArticles] = useState([]);
+  const [selectedArticle, setSelectedArticle] = useState([null]);
+
+  useEffect(() => {
+    fetch('/data/ArticleData.json')
+    .then((response) => response.json())
+    .then((data) => setArticles(data))
+    .catch((error) => console.error('Error!', error));
+  }, []);
+
+  const handleArticleClick= (article) => {
+    setSelectedArticle(article);
+  };
+
+
   return (
     <div className="App">
       <div className='flex-container'>
-
+        <ArticleList articles={articles} onArticleClick={handleArticleClick} />
+        <ArticleText article={selectedArticle} />
       </div>
     </div>
   );
