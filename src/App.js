36ac87388f207764fd0ceb3cc6f0e617d234272e
Vicka <vicrogatinskaya@gmail.com>
2024-01-27 20:54:56 +0100

    feat: added read article

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 
   const [articles, setArticles] = useState([]);
   const [selectedArticle, setSelectedArticle] = useState(null);
+  const [readArticles, setReadArticles] = useState({});
 
   useEffect(() => {
     fetch('/data/ArticleData.json')
@@ -18,12 +19,13 @@ function App() {
 
   const handleArticleClick= (article) => {
     setSelectedArticle(article);
+    setReadArticles({ ...readArticles, [article.id]: true });
   };
 
   return (
     <div className="App">
       <div className='flex-container'>
-        <ArticleList articles={articles} onArticleClick={handleArticleClick} />
+        <ArticleList articles={articles} onArticleClick={handleArticleClick} readArticles={readArticles}/>
         <VerticalLine />
         <ArticleText article={selectedArticle} />
       </div>
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,9 +1,12 @@
-function ArticleList({articles, onArticleClick}) {
+function ArticleList({articles, onArticleClick, readArticles}) {
     return (
       <div className='flex-item1'>
         <ul>
           {articles.map((article, index) => (
-            <li key = {article.id} onClick={() => onArticleClick(article)}>
+            <li key = {article.id} onClick={() => onArticleClick(article)}
+            style={{ 
+                backgroundColor: readArticles[article.id] ? 'rgba(169, 169, 169, 0.5)' : 'transparent' 
+              }}>
               <div className="articleBox">
               <h1>{article.title}</h1>
               <h3>{article.undertitle}</h3>
